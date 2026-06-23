edea64417f20b5854f6a2c0cf97bb240261ec65b
aarangurec <aarangurec@unjbg.edu.pe>
2026-06-22 21:35:42 -0500

    Se inicio el desarrollo del Ejercicio Frecuencias con la lectura de los datos, y la declaración de la cadena

---
+++ Frecuencias.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int P, A, F[100];
+    cout<<"Alumnos: ";
+    cin >> A;
+    cout<<"Profesores: ";
+    cin >> P;
+
+
+
+
+
+
+
+    return 0;
+}
--- Palindromos.cpp
+++ Palindromos.cpp
@@ -26,7 +26,7 @@ for (int i = 0; i < n; i++) {
             cout << "NP\n";
         }
     }
-
+cout<<"Ejemplo";
     return 0;
     }
 
