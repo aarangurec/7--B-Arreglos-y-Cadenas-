c5111ab7b292524c1e508ab3c05ca05d0f67c28c
aarangurec <aarangurec@unjbg.edu.pe>
2026-06-23 19:12:53 -0500

    Primera Prueba en el ejercicio Canicas

--- Canicas.cpp
+++ Canicas.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
 using namespace std;
+int count[1000001];
 int main(){
+    int n, c;
+    cin >> n >> c;
 
+    for(int i = 0; i < c; i++){
+        int v;
+        cin >> v;
+        count[v] = count[v] + 1;
+    }
 
-
-
+    for(int i = 1; i <= n; i++){
+        cout << count[i] << "\n";
+    }
 
 
     return 0;
